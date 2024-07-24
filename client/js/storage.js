622e0201d7225a4175179cd9cdcf8003a6a4f667
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-25 00:28:54 +0300

    Добавил в приложение общее хранилище данных

---
+++ client/js/storage.js
@@ -0,0 +1,31 @@
+export class Storage {
+  static listeners = []
+
+  static todos = [] 
+  static sortingOrder = "asc";
+
+  static getTodos() {
+    return this.todos;
+  }
+  static getSortingOrder() {
+    return this.sortingOrder;
+  }
+
+  static setTodos(todos) {
+    this.todos = todos;
+    this.notityAll();
+  }
+  static setSortingOrder(order) {
+    this.sortingOrder = order;
+  }
+
+  static addListener(func) {
+    this.listeners.push(func);
+  }
+
+  static notityAll() {
+    for (const listener of this.listeners) {
+      listener(this.todos);
+    }
+  }
+}

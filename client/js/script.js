dfba5c81e66db8be3b7cf9eba44620ca3b4b0dd6
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-25 00:28:34 +0300

    Обновил основной js файл

--- client/js/script.js
+++ client/js/script.js
@@ -1,23 +1,35 @@
 import { getAllTodos } from "./api.js";
+import { onSortButtonClicked, onTodayTodosButtonClick, onUncompletedTodosCheckbox, onWeekTodosButtonClick } from "./handlers.js";
+import { Storage } from "./storage.js";
+import { displayToDoList } from "./ui.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Создаём календарь
   $(function() {
     $("#datepicker").datepicker({ inline: true });
   });
 
-  $( function() {
+  // Создаём кнопку для выбора только незавершённых задач
+  $(function() {
     $("input[type='checkbox']").checkboxradio();
   });
 
+  // Запрашиваем все задачи и заносим их в хранилище
+  getAllTodos().then((res) => Storage.setTodos(res));
 
-  const todosList = document.getElementById("todos-list");
-  getAllTodos()
-    .then((res) => {
-      res.forEach((todo) => {
-        const li = document.createElement("li");
-        li.textContent = todo.name;
-        todosList.appendChild(li);
-      });
-    })
+  // Добавляем слушателя, который при обновлении списка задач выводит обновлённый список на экран
+  Storage.addListener((todos) => {
+    displayToDoList('todo-container', todos);
+  });
+
+  // Ставим текущую дату в нужное поле на интерфейсе
+  $(".today-date").text(new Date().toISOString().split("T")[0]);
+
+  // Ставим слушателей на различные элементы страницы
+  document.getElementById("is_uncompleted").addEventListener('click', onUncompletedTodosCheckbox);
+  document.getElementById("today-todos-button").addEventListener('click', onTodayTodosButtonClick);
+  document.getElementById("week-todos-button").addEventListener('click', onWeekTodosButtonClick);
+  document.getElementById("todo-sorting").addEventListener('click', onSortButtonClicked);
+  
 })
 

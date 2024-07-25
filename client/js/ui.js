80316ec0e2cd9c68ac820cc60e957b5c3111f67e
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-25 13:16:35 +0300

    Обновление логики создания компонента для отображения задачи + добавление комментариев

--- client/js/ui.js
+++ client/js/ui.js
@@ -1,3 +1,8 @@
+/**
+ * Отображение списка задач в определённый html-элемент
+ * @param {string} elementId Идентификатор элемента, в который помещается список
+ * @param {any[]} todoList Массив объектов с информацией о задачах
+ */
 export function displayToDoList(elementId, todoList) {
   const element = $(`#${elementId}`);
   element.empty();
@@ -8,8 +13,20 @@ export function displayToDoList(elementId, todoList) {
   });  
 }
 
+/**
+ * Создание div-контейнера с информацией о конкретной задаче
+ * @param {any} todoData Информация о конкретной задаче
+ * @returns html-элемент, заполненный информацией о переданной задаче
+ */
 function createItem(todoData) {
   const $todoDiv = $("<div>", {"class": "todo-block"})
+    .click(function() {
+      $("#modal-todo-name").text(todoData.name);
+      $("#modal-todo-status").attr("src", todoData.status ? 'styles/images/completed_task.svg' : 'styles/images/uncompleted_task.svg');
+      $("#modal-todo-datetime").text(formatDate(new Date(todoData.date)));
+      $("#modal-todo-desc").text(todoData.fullDesc);
+      $("#dialog").dialog("open");
+    })
   const $todoText = $("<div>", {"class": "todo-text"})
     .append($("<b>").text(todoData.name))
     .append($("<p>").text(todoData.shortDesc))
@@ -25,6 +42,38 @@ function createItem(todoData) {
   return $todoDiv;
 }
 
+/**
+ * Отображение уведомления о загрузке данных
+ */
+export function showLoadingMessage() {
+  $("#loading-message").css({ display: 'block' });
+}
+/**
+ * Скрытие уведомления о загрузке данных
+ */
+export function hideLoadingMessage() {
+  $("#loading-message").css({ display: 'none' });
+}
+
+/**
+ * Отображение уведомления об ошибке
+ * @param {string} message Содержание ошибки
+ */
+export function showErrorMessage(message) {
+  $("#error-message").css({ display: 'block' }).text(message);
+}
+/**
+ * Скрытие уведомления об ошибке
+ */
+export function hideErrorMessage() {
+  $("#error-message").css({ display: 'none' }).text("");
+} 
+
+/**
+ * Форматирование даты в строку вида "dd.MM.uuuu HH:mm"
+ * @param {Date} date Дата, которую необходимо отформматировать
+ * @returns Строка вида "dd.MM.uuuu HH:mm"
+ */
 function formatDate(date) {
   const pad = (num) => num < 10 ? '0' + num : num;
 

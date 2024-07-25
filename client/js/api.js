511297bb45ac3112118f3cc9899b7b282f8c8df7
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-25 13:13:57 +0300

    Обновление логики взаимодействия с сервером + добавление комментариев

--- client/js/api.js
+++ client/js/api.js
@@ -19,47 +19,55 @@ export async function getAllTodos() {
  * Получение списка дел за сегодняшний день
  * @returns Список todo дел, которые в поле date имеют дату, равнюю сегоднящнему дню
  */
-export async function getTodayTodos() {
-  const todayDate = new Date();
-  todayDate.setHours(0, 0, 0, 0);
-  
-  const tommorowDate = new Date(todayDate);
-  tommorowDate.setDate(todayDate.getDate() + 1);
-  tommorowDate.setHours(0, 0, 0, 0)
-
-  const response = await fetch(`${url}/date?from=${todayDate.getTime()}&to=${tommorowDate.getTime()}`);
-
-  if (!response.ok) {
-    const error = await response.json();
-    throw new Error(`Возникла ошибка во время получения объектов за текущий день: ${error.message || response.statusText}`);
-  }
-
-  return response.json();
-}
-
 export async function getTodosToday() {
   const today = new Date();
   
   const tommorow = new Date(today);
   tommorow.setDate(tommorow.getDate() + 1);
 
-  return getTodosBetween(today, tommorow);
+  return getTodosBetweenDays(today, tommorow);
 }
 
+/**
+ * Получение списка дел на текущую неделю
+ * @returns Список todo дел, которые в поле data имеют даты, входящую в текущую неделю
+ */
 export async function getTodosWeek() {
   const today = new Date();
 
   const oneWeekLater = new Date(today)
   oneWeekLater.setDate(oneWeekLater.getDate() + 7);
 
-  return getTodosBetween(today, oneWeekLater);
+  return getTodosBetweenDays(today, oneWeekLater);
 }
 
-async function getTodosBetween(fromDate, toDate) {
+/**
+ * Получение списка дел по их названию или по его части
+ * @param {string} name Полное название предмета или его часть
+ * @returns Список дел, название которых полностью или частично совпадает с вводом
+ */
+export async function getTodosByName(name) {
+  const response = await fetch(`${url}/find?q=${name}`)
+  
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(`Возникла ошибка во время получения объектов по содержимому: ${error.message || response.statusText}`)
+  }
+
+  return response.json(); 
+}
+
+/**
+ * Получение списка дел в определённый промежуток дней
+ * @param {Date} fromDate День, начиная с которого необходимо найти задания
+ * @param {Date} toDate День, по который надо найти задания
+ * @returns Все todo, у которых поле date попадает в заданный промежуток
+ */
+export async function getTodosBetweenDays(fromDate, toDate) {
   fromDate.setHours(0, 0, 0, 0);
   toDate.setHours(0, 0, 0, 0);
 
-  const response = await fetch(`${url}/date?from=${fromDate.getTime()}&to=${toDate.getTime()}`)
+  const response = await fetch(`${url}/date?from=${fromDate.getTime()}&to=${toDate.getTime()}`);
 
   if (!response.ok) {
     const error = await response.json();

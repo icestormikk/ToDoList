edc687f4ef840a3b0c088feaf2e6b356c278e6ee
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-25 13:15:33 +0300

    Большое обновление функций для инициализации элементов и их обработчиков

--- client/js/script.js
+++ client/js/script.js
@@ -1,12 +1,63 @@
 import { getAllTodos } from "./api.js";
-import { onSortButtonClicked, onTodayTodosButtonClick, onUncompletedTodosCheckbox, onWeekTodosButtonClick } from "./handlers.js";
+import { 
+  onPickedDateCalenadarUpdate, 
+  onPickedDateRangeCalendarUpdate, 
+  onSearchbarUpdate, 
+  onSortButtonClicked, 
+  onTodayTodosButtonClick, 
+  onUncompletedTodosCheckbox, 
+  onWeekTodosButtonClick 
+} from "./handlers.js";
 import { Storage } from "./storage.js";
-import { displayToDoList } from "./ui.js";
+import { displayToDoList, hideLoadingMessage, showErrorMessage, showLoadingMessage } from "./ui.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+  let from = null, to = null;
+
   // Создаём календарь
-  $(function() {
-    $("#datepicker").datepicker({ inline: true });
+  $("#datepicker").datepicker({
+    inline: true,
+    numberOfMonths: 1,
+    onSelect: function(dateText, inst) {
+      const date = $(this).datepicker('getDate');
+
+      if (!from || to) {
+          from = date;
+          to = null;
+      } else {
+          if (date < from) {
+            to = from;
+            from = date;
+          } else {
+            to = date;
+          }
+      }
+
+      // если выбран только один день
+      if (from && !to) {
+        onPickedDateCalenadarUpdate(from);
+      }
+      // если указан конец и начало диапазона
+      if (from && to) {
+        onPickedDateRangeCalendarUpdate(from, to);
+      }
+    },
+    // стилизация ячеек, оказавшихся внутри промежутка 
+    beforeShowDay: function(date) {
+      if (!from || !to) {
+        return [true, ""];
+      }
+      
+      let cssClass = "";
+      if (from && !to && date.getTime() === from.getTime()) {
+          cssClass = "selected-start";
+      } else if (to && date.getTime() === to.getTime()) {
+          cssClass = "selected-end";
+      } else if (from && to && date >= from && date <= to) {
+          cssClass = "range";
+      }
+      return [true, cssClass];
+    }
   });
 
   // Создаём кнопку для выбора только незавершённых задач
@@ -15,7 +66,11 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Запрашиваем все задачи и заносим их в хранилище
-  getAllTodos().then((res) => Storage.setTodos(res));
+  showLoadingMessage()
+  getAllTodos()
+    .then((res) => Storage.setTodos(res))
+    .catch((err) => showErrorMessage(err))
+    .finally(() => hideLoadingMessage());
 
   // Добавляем слушателя, который при обновлении списка задач выводит обновлённый список на экран
   Storage.addListener((todos) => {
@@ -25,11 +80,33 @@ document.addEventListener('DOMContentLoaded', () => {
   // Ставим текущую дату в нужное поле на интерфейсе
   $(".today-date").text(new Date().toISOString().split("T")[0]);
 
+  // Создание и стилизация диалогового окна
+  $("#dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    width: '400px',
+    open: function() {
+        $(".ui-widget-overlay").css({ background: 'rgba(0,0,0,0.8)', opacity: 0.5 });
+        $(".ui-dialog-titlebar").hide();
+        // Отключаем скролл окна при открытии
+        $("body").css({ overflow: 'hidden' })
+    },
+    close: function () {
+        // Включаем скролл окна при закрытии модального окна
+        $("body").css({ overflow: '' })
+    },
+    buttons: {
+      "Готово": function() {
+          $(this).dialog("close");
+      }
+    }
+  });
+
   // Ставим слушателей на различные элементы страницы
   document.getElementById("is_uncompleted").addEventListener('click', onUncompletedTodosCheckbox);
   document.getElementById("today-todos-button").addEventListener('click', onTodayTodosButtonClick);
   document.getElementById("week-todos-button").addEventListener('click', onWeekTodosButtonClick);
   document.getElementById("todo-sorting").addEventListener('click', onSortButtonClicked);
-  
+  document.getElementById("searchbar").addEventListener('change', onSearchbarUpdate)
 })
 
